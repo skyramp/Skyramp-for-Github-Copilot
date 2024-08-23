4946f257b5f43611b6987da3cae51fb9acc5476b
Shiny Chimra <shinychimra07@gmail.com>
2024-08-23 10:37:42 -0700

    changing reference

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# install sample-microservices and skyramp
+
+# install skyramp
+yes | bash -c "$(curl -fsSL https://skyramp.dev/installer.sh)"
+
+# install skyramp worker and dashboard
+curl -o /etc/skyramp/docker-compose.yml https://raw.githubusercontent.com/letsramp/Skyramp-for-Github-Copilot/main/.devcontainer/scripts/docker-compose.yml
 docker compose -f /etc/skyramp/docker-compose.yml up -d --wait || true
+
 # start skyramp  server
 nohup skyramp server up -vvv &
+
 # open public port for skyramp dashboard
 gh codespace ports visibility 4000:public -c $CODESPACE_NAME
 gh codespace ports visibility 3000:public -c $CODESPACE_NAME
-# install python3.11 and requirements
-apt-get update && apt-get install -y python3.11 python3-pip && update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
-pip3 install -r /etc/skyramp/requirements.txt
+
+# install python requirements
+curl -o /etc/skyramp/requirements.txt https://raw.githubusercontent.com/letsramp/Skyramp-for-Github-Copilot/main/.devcontainer/scripts/requirements.txt
+chmod +x /etc/skyramp/requirements.txt
+/usr/local/python/current/bin/pip install -r /etc/skyramp/requirements.txt
